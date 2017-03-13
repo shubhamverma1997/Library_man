2587219e009fd8231c42003c10c972e36118bd22
Shubham Verma <lifemeansmore008@gmail.com>
2017-03-13 22:27:40 +0530

    Add files via upload

---
+++ LibManSys/src/javaconnect.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubham Verma
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    Connection conn;
+
+    public static Connection ConnecrDb()
+    {
+        try
+        {
+            
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+        }
+    }
+    }
+}   
